73708a41551b970f17af67c9388b25180794ae00
aeshabb <belalesa725@gmail.com>
2024-05-18 01:52:34 +0300

    Filters has removed by aeshabb

--- src/main/java/su/arlet/finance_hack/services/AuthService.java
+++ src/main/java/su/arlet/finance_hack/services/AuthService.java
@@ -4,17 +4,18 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 import su.arlet.finance_hack.core.User;
+import su.arlet.finance_hack.exceptions.InvalidAuthorizationHeaderException;
 import su.arlet.finance_hack.exceptions.UserAlreadyExistsException;
 import su.arlet.finance_hack.exceptions.UserNotFoundException;
 import su.arlet.finance_hack.exceptions.WrongPasswordException;
 import su.arlet.finance_hack.repos.UserRepo;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 @Service
@@ -66,4 +67,14 @@ public class AuthService {
     }
 
 
+    public String getUsernameByHttpRequest(HttpServletRequest req) {
+        String auth = req.getHeader("Authorization");
+
+        if (auth != null) {
+            String[] parsedHeader = auth.split(" ");
+            if (parsedHeader.length == 2 && (parsedHeader[0].equalsIgnoreCase("bearer")))
+                return decodeJwtToken(parsedHeader[1]);
+        }
+        throw new InvalidAuthorizationHeaderException();
+    }
 }
