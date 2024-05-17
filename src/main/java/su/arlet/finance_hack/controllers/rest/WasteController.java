4033e26f36b635a7d22ea2a0b8ffb4bb943ba51e
zotov <zotov@protei.ru>
2024-05-18 02:53:46 +0300

    add WasteController

--- src/main/java/su/arlet/finance_hack/controllers/rest/WasteController.java
+++ src/main/java/su/arlet/finance_hack/controllers/rest/WasteController.java
@@ -2,6 +2,9 @@ package su.arlet.finance_hack.controllers.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import su.arlet.finance_hack.core.PaymentInfo;
 import su.arlet.finance_hack.core.User;
+import su.arlet.finance_hack.exceptions.InvalidPaymentInfoException;
 import su.arlet.finance_hack.exceptions.UserNotFoundException;
+import su.arlet.finance_hack.exceptions.UsernameNotSetException;
 import su.arlet.finance_hack.services.AuthService;
 import su.arlet.finance_hack.services.PaymentInfoService;
 
@@ -20,6 +25,12 @@ import su.arlet.finance_hack.services.PaymentInfoService;
 @Tag(name = "WASTE API")
 public class WasteController {
 
+    private class PaymentInfoEntity {
+
+        private PaymentInfo info;
+        private String username;
+
+    }
 
     private final PaymentInfoService paymentInfoService;
     private final AuthService authService;
@@ -30,9 +41,18 @@ public class WasteController {
         this.authService = authService;
     }
 
-    public ResponseEntity<?> createWaste(@RequestBody PaymentInfo info, HttpServletRequest servletRequest) {
-        String username = authService.getUsernameByHttpRequest(servletRequest);
-        info.setUser(authService.getByUsername(username));
+    public ResponseEntity<?> createWaste(@RequestBody PaymentInfoEntity infoEntity) {
+
+        if (infoEntity.username == null) {
+            throw new UsernameNotSetException();
+        }
+
+        User user = authService.getByUsername(infoEntity.username);
+        PaymentInfo info = infoEntity.info;
+        if (info == null)
+            throw new InvalidPaymentInfoException();
+
+        info.setUser(user);
         return new ResponseEntity<>(paymentInfoService.addWaste(info), HttpStatus.OK);
     }
 
@@ -44,6 +64,7 @@ public class WasteController {
         if (!paymentInfo.getUser().getUsername().equals(username)) {
             throw new UserNotFoundException();
         }
+
 
     }
 
---
+++ src/main/java/su/arlet/finance_hack/exceptions/UsernameNotSetException.java
@@ -0,0 +1,4 @@
+package su.arlet.finance_hack.exceptions;
+
+public class UsernameNotSetException extends RuntimeException{
+}
