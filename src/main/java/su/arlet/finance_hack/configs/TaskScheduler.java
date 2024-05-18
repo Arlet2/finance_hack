a23e8f06c0c598922c28dd674efe0c0c9bcb14ef
arlet <shulga@niuitmo.ru>
2024-05-18 16:37:14 +0300

    fixes

---
+++ src/main/java/su/arlet/finance_hack/configs/TaskScheduler.java
@@ -0,0 +1,18 @@
+package su.arlet.finance_hack.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class TaskScheduler {
+    @Bean
+    public org.springframework.scheduling.TaskScheduler threadPoolTaskScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler
+                = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix(
+                "ThreadPoolTaskScheduler");
+        return threadPoolTaskScheduler;
+    }
+}
